b239dcb262e91b870228e4868eb3d629b3977736
Ben Wallberg <wallberg@umd.edu>
2023-11-16 05:10:14 -0500

    Replace Graph with ConjunctiveGraph
    Also distinguish document sources from subjects

--- export.py
+++ export.py
@@ -1,13 +1,26 @@
 #!/usr/bin/env python3
 
-from rdflib import Graph
+from rdflib import Graph, ConjunctiveGraph
+from rdflib.namespace import Namespace
+
+# For some reason, switching to ConjunctiveGraph seems to
+# preserve discovered namespace prefix mapping from the
+# parse(). These probably aren't necessary, but leaving
+# them here for now.
+
+PCDM = Namespace("http://pcdm.org/models#")
+LDP = Namespace("http://www.w3.org/ns/ldp#")
+REL = Namespace("http://id.loc.gov/vocabulary/relators/")
+IANA = Namespace("http://www.iana.org/assignments/relation/")
+EDM = Namespace("http://www.europeana.eu/schemas/edm/")
+FEDORA = Namespace("http://fedora.info/definitions/v4/repository#")
+BIBO = Namespace("http://purl.org/ontology/bibo/")
 
 # Create a Graph
-g = Graph('BerkeleyDB', identifier='graph')
-
-g.open('graph', create=True)
+g = ConjunctiveGraph('BerkeleyDB')
+g.open('graph')
 
 # Print out the combined Graph in the RDF Turtle format
-print(g.serialize(format="ntriples"))
+print(g.serialize(format="turtle"))
 
 g.close()
--- fetch.py
+++ fetch.py
@@ -1,40 +1,51 @@
 #!/usr/bin/env python3
 
-from rdflib import Graph
+from rdflib import Graph, ConjunctiveGraph
+from rdflib.term import URIRef
 
-sources = [
-    "https://fcrepo.lib.umd.edu/fcrepo/rest/dc/2023/1/0a/d9/1a/5e/0ad91a5e-4111-45ec-aba8-42ab3651213f",
-    "https://fcrepo.lib.umd.edu/fcrepo/rest/pcdm/04/14/ef/d2/0414efd2-a519-48fd-b361-28c79082f58b",
-    "http://vocab.lib.umd.edu/form#records",
-    "http://vocab.lib.umd.edu/collection#0068-LBR-RG9-003",
+# Initial list of subjects to populate the graph
+subjects = [
+    URIRef("https://fcrepo.lib.umd.edu/fcrepo/rest/dc/2023/1/0a/d9/1a/5e/0ad91a5e-4111-45ec-aba8-42ab3651213f"),
+    URIRef("https://fcrepo.lib.umd.edu/fcrepo/rest/pcdm/04/14/ef/d2/0414efd2-a519-48fd-b361-28c79082f58b"),
 ]
 
-# Create or open a Graph with Berkeley DB persistence store
-g = Graph('BerkeleyDB', identifier='graph')
+# Set of subject base patterns we are willing to follow from
+# discovered objects
+follows = set([
+    # "https://fcrepo.lib.umd.edu/fcrepo/",
+    URIRef("http://vocab.lib.umd.edu/"),
+])
 
+# Create or open a Graph with Berkeley DB persistence store
+g = ConjunctiveGraph('BerkeleyDB')
 g.open('graph', create=True)
+
 print(f'Triples at start: {len(g)}')
 
-for source in sources:
+# Document sources we have already fetched
+sources = set([subject.defrag() for subject in g.subjects(predicate=None, object=None, unique=True)])
 
-    # Determine if this source already exists in the graph
-    q = f"""
-    SELECT ?o
-    WHERE {{
-        <{source}> ?p ?o .
-    }}
-"""
+i = 0
+while i < len(subjects):
+    subject = subjects[i]
+    source = subject.defrag()
 
-    in_graph = False
-    for r in g.query(q):
-        in_graph = True
-        break
-
-    if not in_graph:
+    if source not in sources:
         # Add this source to the graph
         print(f'Adding {source=}')
 
         g.parse(source=source)
+
+    # Determine if the new triples have an object which
+    # we want to follow
+    for _, _, object in g.triples((subject, None, None)):
+        if isinstance(object, URIRef):
+            newsubject = object
+            if newsubject not in subjects and any(newsubject.startswith(follow) for follow in follows):
+                subjects.append(newsubject)
+
+    i += 1
+
 
 print(f'Triples at end: {len(g)}')
 
