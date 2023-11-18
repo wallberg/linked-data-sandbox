93f6217af96a6627515ac47da2dd3fc3c25ddaf8
Ben Wallberg <wallberg@umd.edu>
2023-11-18 17:09:30 -0500

    Update aspace/fetch.sh

--- aspace/fetch.sh
+++ aspace/fetch.sh
@@ -4,14 +4,15 @@
 
 # Spiro Agnew Papers
 #
-# Includes internal references to:
-#    schema:creator <https://archives.lib.umd.edu//agents/people/5256>
-#    schema:holdingArchive <https://archives.lib.umd.edu//repositories/2>
-#
-# The information included for subject https://archives.lib.umd.edu//repositories/2 in the document
-# https://archives.lib.umd.edu/repositories/2/resources/963 does not include everything returned in
-# the document https://archives.lib.umd.edu/repositories/2/resources/963 .
 
 any23 rover -e html-embedded-jsonld -f turtle https://archives.lib.umd.edu/repositories/2/resources/963 > agnew-papers.ttl
+
+# Includes internal references to:
+#  Spiro Agnew:  schema:creator <https://archives.lib.umd.edu//agents/people/5256>
+#  SCUA:         schema:holdingArchive <https://archives.lib.umd.edu//repositories/2>
+#
+# The information included for subject https://archives.lib.umd.edu/repositories/2/resources/963 is a subset of
+# what is included when you request the document https://archives.lib.umd.edu/repositories/2/resources/963 .
+
 any23 rover -e html-embedded-jsonld -f turtle https://archives.lib.umd.edu/agents/people/5256 > agnew.ttl
 any23 rover -e html-embedded-jsonld -f turtle https://archives.lib.umd.edu/repositories/2 > scua.ttl
