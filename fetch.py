6fbd61b084d84908dbb591f62991e534ee5e8934
Ben Wallberg <wallberg@umd.edu>
2024-05-23 12:55:26 -0400

    Add explicit format to fetch
    And add a DRUM and wikidata source

--- fetch.py
+++ fetch.py
@@ -6,8 +6,10 @@ from rdflib.store import Store
 
 # Initial list of subjects to populate the graph
 subjects = [
-    URIRef("https://fcrepo.lib.umd.edu/fcrepo/rest/dc/2023/1/0a/d9/1a/5e/0ad91a5e-4111-45ec-aba8-42ab3651213f"),
-    URIRef("https://fcrepo.lib.umd.edu/fcrepo/rest/pcdm/04/14/ef/d2/0414efd2-a519-48fd-b361-28c79082f58b"),
+    (URIRef("https://www.wikidata.org/entity/Q7730092"), None),
+    (URIRef("https://drum.lib.umd.edu/items/f4d2184d-4bde-46aa-8477-25e8605c8cc4"), "json-ld"),
+    (URIRef("https://fcrepo.lib.umd.edu/fcrepo/rest/dc/2023/1/0a/d9/1a/5e/0ad91a5e-4111-45ec-aba8-42ab3651213f"), None),
+    (URIRef("https://fcrepo.lib.umd.edu/fcrepo/rest/pcdm/04/14/ef/d2/0414efd2-a519-48fd-b361-28c79082f58b"), None),
 ]
 
 # Set of subject base patterns we are willing to follow from
@@ -27,18 +29,23 @@ g.open(path, create=False)
 print(f'Triples at start: {len(g)}')
 
 # Document sources we have already fetched
-sources = set([subject.defrag() for subject in g.subjects(None, None, True)])
+sources = set([
+    subject.defrag() if isinstance(subject, URIRef) else subject
+    for subject
+    in g.subjects(None, None, True)
+])
 
 i = 0
 while i < len(subjects):
     subject = subjects[i]
-    source = subject.defrag()
+    source = subject[0].defrag()
+    format = subject[1]
 
-    if source not in sources:
-        # Add this source to the graph
-        print(f'Adding {source=}')
+    # if source not in sources:
+    #     # Add this source to the graph
+    #     print(f'Adding {source=}')
 
-        g.parse(source=source)
+    g.parse(source=source, format=format)
 
     # Determine if the new triples have an object which
     # we want to follow
@@ -51,6 +58,8 @@ while i < len(subjects):
     i += 1
 
 
+# Print out the combined Graph in the RDF Turtle format
+#print(g.serialize(format="turtle"))
 print(f'Triples at end: {len(g)}')
 
 g.close()
