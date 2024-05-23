4a9b05ee75f897079bfda033e20973d313d5a0c7
Ben Wallberg <wallberg@umd.edu>
2024-05-23 20:30:13 +0000

    add dump.py

---
+++ dump.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+from os import path
+
+from rdflib import plugin, Dataset
+from rdflib.term import URIRef
+from rdflib.store import Store
+
+from config import Config
+
+config = Config()
+
+store_db = config.config["store_db"]
+
+# Create or open a Graph with Level DB persistence store
+store = plugin.get("LevelDB", Store)(identifier=URIRef(store_db["identifier"]))
+
+dbpath = store_db["file"]
+
+ds = Dataset(store)
+ds.open(dbpath, create=False)
+
+print(ds.serialize(format="trig"))
