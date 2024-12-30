c56aed9584a28465afa41d0e5a0d5e3e527e1bb2
Ben Wallberg <wallberg@umd.edu>
2024-12-30 11:11:37 -0500

    wip: add aspace objects

---
+++ aspace/aspace-fetch-objects.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+
+from pathlib import Path
+
+from pprint import pprint
+
+from rdflib import Dataset, Namespace, XSD
+from rdflib.namespace import RDF
+from rdflib.term import URIRef, BNode, Literal
+
+from asnake.aspace import ASpace
+import asnake.logging as logging
+
+from dotenv import load_dotenv
+
+import requests
+import xmltodict
+
+# Add any environment variables from .env
+load_dotenv('../.env')
+
+# Get environment variables
+env = {}
+for key in ('ASPACE_API_URL', 'ASPACE_API_USERNAME',
+            'ASPACE_API_PASSWORD', 'ASPACE_PUBLIC_URL'):
+    env[key] = os.environ.get(key)
+    if env[key] is None:
+        raise RuntimeError(f'Must provide environment variable: {key}')
+
+debug=False
+
+SCHEMA = Namespace("http://schema.org/")
+ASPACE = Namespace("https://archives.lib.umd.edu/")
+
+ds = Dataset()
+ds.bind("schema", SCHEMA)
+ds.bind("aspace", ASPACE)
+
+g = ds.graph(ASPACE)
+
+# Setup the ArchivesSnake library
+config = {
+    'baseurl': env['ASPACE_API_URL'],
+    'username': env['ASPACE_API_USERNAME'],
+    'password': env['ASPACE_API_PASSWORD'],
+    'default_config': 'INFO_TO_STDERR',
+}
+
+logger = logging.get_logger("sitemap")
+if debug:
+    logging.setup_logging(stream=sys.stderr, level="DEBUG")
+else:
+    logging.setup_logging(stream=sys.stderr, level="INFO")
+
+logger.debug(f'config={config}')
+
+logger.info("Begin generating sitemap.xml")
+
+aspace = ASpace(**config)
+
+# Authorize as the provided username
+aspace.authorize()
+
+# Iterate over published repositories
+for repo in aspace.repositories:
+    if repo.publish:
+
+        repo_uri = URIRef(ASPACE + repo.uri)
+
+        # Iterate over published digital objects
+        for digital_object in repo.digital_objects:
+
+            # TODO: use file_version instead of representative_file_version?
+            if digital_object.publish and hasattr(digital_object, "representative_file_version"):
+
+                object_uri = URIRef(ASPACE + digital_object.uri)
+
+                logger.info(f"{object_uri=}")
+
+                if debug:
+                    pprint(vars(digital_object))
+
+                g.add((object_uri, RDF.type, URIRef(SCHEMA + "ArchiveComponent")))
+                g.add((object_uri, URIRef(SCHEMA + "holdingArchive"), repo_uri))
+                g.add((object_uri, URIRef(SCHEMA + "name"), Literal(digital_object.title)))
+                g.add((object_uri, URIRef(SCHEMA + "dateCreated"),
+                    Literal(digital_object.representative_file_version.create_time, datatype=XSD.dateTime)))
+                g.add((object_uri, URIRef(SCHEMA + "dateModified"),
+                    Literal(digital_object.representative_file_version.user_mtime, datatype=XSD.dateTime)))
+
+                for collection in digital_object.collection:
+                    g.add((object_uri, URIRef(SCHEMA + "isPartOf"), URIRef(ASPACE + collection.ref)))
+
+                g.add((object_uri, URIRef(SCHEMA + "thumbnailUrl"), Literal(digital_object.representative_file_version.file_uri)))
+                g.add((object_uri, URIRef(SCHEMA + "sameAs"), Literal(digital_object.representative_file_version.link_uri)))
+                g.add((object_uri, URIRef(SCHEMA + "url"), Literal(digital_object.representative_file_version.link_uri)))
+
+                # creator?
+
+                # break
+
+# print(ds.serialize(format="trig"))
+ds.serialize(destination=Path("./aspace-objects.trig"), format="trig")
