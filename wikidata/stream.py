6b9843b8ecf52cda1bd1adbaca81492b8fe79d7b
Ben Wallberg <wallberg@umd.edu>
2025-09-21 07:27:11 -0400

    fix: add User-Agent to wikidata activity stream
    * fixes 403 forbidden error
    * also remove unnecessary sseclient package

--- wikidata/stream.py
+++ wikidata/stream.py
@@ -1,19 +1,27 @@
-#!/usr/bin/env python3
-
+import requests
 import json
 
-from sseclient import SSEClient as EventSource
+url = "https://stream.wikimedia.org/v2/stream/recentchange"
 
-url = 'https://stream.wikimedia.org/v2/stream/recentchange'
-for event in EventSource(url):
-    if event.event == 'message':
-        try:
-            change = json.loads(event.data)
-        except ValueError:
-            pass
-        else:
-            # print("========")
-            # if change['user'] == 'InternetArchiveBot':
-            print('{user} edited {title_url}'.format(**change))
-            # for key, value in change.items():
-            #     print(f' {key=}, {value=}')
+headers = {
+    'User-Agent': 'LinkedDataSandbox/0.1 (https://github.com/wallberg/linked-data-sandbox)'
+}
+
+with requests.get(url, stream=True, headers=headers) as resp:
+    resp.raise_for_status()
+    for line in resp.iter_lines():
+        if line:
+            decoded_line = line.decode('utf-8')
+            if decoded_line.startswith('data: '):
+                data = decoded_line[6:]
+                event = json.loads(data)
+                # print(event)
+                print("========")
+                # if change['user'] == 'InternetArchiveBot':
+                # print('{user} edited {title_url}'.format(**event))
+                for key, value in event.items():
+                    if key == 'meta':
+                        for mkey, mvalue in value.items():
+                            print(f'  meta.{mkey=}, {mvalue=}')
+                    else:
+                        print(f' {key=}, {value=}')
