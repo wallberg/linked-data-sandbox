576368da1947aa9ef1ec6bf5823a54711d1407e3
Ben Wallberg <wallberg@umd.edu>
2025-11-02 06:20:45 -0500

    feat: remove unused Java

--- BenWallberg/fetch.bash
+++ BenWallberg/fetch.bash
@@ -7,12 +7,6 @@
 
 python -m rdflib.tools.rdfpipe -i turtle -o turtle https://wallberg.solidcommunity.net/profile/card#me > solid.ttl
 
-
-# Fetch schema.org information from the Libraries' Staff Directory
-# This loses the subject
-# The Libraries' Website staff directory entry no longer contains embedded json-ld
-#any23 rover -e html-embedded-jsonld -f turtle -o staff-directory.ttl https://www.lib.umd.edu/directory/staff/wallberg
-
 # Fetch ORCID public information
 
 python -m rdflib.tools.rdfpipe -i turtle -o turtle https://orcid.org/0000-0002-4904-005X > orcid.ttl
