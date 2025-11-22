7d688764c79440b95c13d6c5273f30cf4114b19a
Ben Wallberg <wallberg@umd.edu>
2025-11-22 10:55:22 -0500

    feat: add caching for resumability

--- orcid/orcid.py
+++ orcid/orcid.py
@@ -4,6 +4,7 @@ from rdflib import Dataset
 
 import requests
 
+CACHE_DIR = Path.home() / "git" / "linked-data-sandbox" / ".cache" / "orcid"
 
 def query_orcid(query):
     """ Query the ORCID public API and return a set of ORCID iDs matching the query.
@@ -69,16 +70,43 @@ ds = Dataset()
 
 g = ds.graph("https://orcid.org/")
 
+# Download each ORCID iD, caching results so the
+# process is restartable (due to long download times)
+CACHE_DIR.mkdir(parents=True, exist_ok=True)
+orcid_ids = sorted(orcid_ids)
 for orcid_id, n in zip(orcid_ids, range(len(orcid_ids))):
-    print(f"Processing ORCID iD: {orcid_id}")
+    cache_file = CACHE_DIR / f"{orcid_id[18:]}.trig"
 
-    # ORCID Profile
-    g.parse(source=orcid_id, format="turtle")
+    if cache_file.exists():
+        print(f"Skipping cached ORCID iD: {orcid_id}")
 
-    # ORCID Works
-    g.parse(source=orcid_id, format="json-ld")
+    else:
+        print(f"Downloading ORCID iD: {orcid_id}")
 
-    if n % 100 == 0:
-        print(f"--- Processed {n} ORCID iDs ---")
+        try:
+            # ORCID Profile
+            g.parse(source=orcid_id, format="turtle")
+
+            # ORCID Works
+            g.parse(source=orcid_id, format="json-ld")
+
+            ds.serialize(destination=cache_file, format="trig")
+
+        except Exception as e:
+            print(f"Error serializing ORCID iD {orcid_id}: {e}")
+
+            # Remove the malformed cache file if it exists
+            cache_file.unlink(missing_ok=True)
+
+        # Clear the graph
+        for s, p, o in list(g):
+            g.remove((s, p, o))
+
+    if n % 99 == 0:
+        print(f"--- Completed {n+1} ORCID iDs ---")
+
+# Combine cached files into final dataset
+for cache_file in CACHE_DIR.glob("*.trig"):
+    g.parse(source=cache_file, format="trig")
 
 ds.serialize(destination=Path("orcid.trig"), format="trig")
